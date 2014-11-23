3662ac1120d95500d734148d14bd6dabd3e984a0
Zhen <zhenw.wang@gmail.com>
2014-11-23 15:34:29 -0500

    Add html

--- templates.py
+++ templates.py
@@ -2,12 +2,21 @@ import os
 
 import webapp2
 
+form_html = """
+<form>
+<h2>Add a Food</h2>
+<input type="text" name="food">
+<input type="hidden" name="food" value="eggs">
+<button>Add</button>
+</form>
+"""
+
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
 
 class MainPage(Handler):
 	def get(self):
-		self.write("Hello world")
+		self.write(form_html)
 
 app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
